726f38f090c03d1f4901d65e4c8d2fde7bf1a9da
DanGrund <hello@dangrund.com>
2016-10-11 14:04:31 -0600

    the number guess works

--- guess.js
+++ guess.js
@@ -1,37 +1,49 @@
+//identify the input field
 var userNumber = document.getElementById("number");
-
+//identify the 'last guess' output area
 var printNumber = document.querySelector('h2');
-
+//identify the 'guess' button
 var guess = document.getElementById('guess');
-
-guess.addEventListener('click', function(){
+//function to collect the value from input field and store it as variable 'userNumber'
+function collectUserNumber () {
   var lastGuess = userNumber.value;
   printNumber.innerText = lastGuess;
-  compNumber();
-  compare();
-});
-
-var numberOne = function(){
+}
+//convert value of userNumber to numeric from string, store as new variable numberOne
+ function numberOne() {
   return parseInt(userNumber.value);
-};
-
-var compNumber = function(){
+}
+//on 'guess' button click, run collectUserNumber
+guess.addEventListener('click', collectUserNumber);
+// generate random number to variable compNumber
+ function compNumber(){
    var rand = Math.floor((Math.random()*100)+1);
    return rand;
- };
-console.log(numberOne());
-console.log(compNumber());
+ }
+ var randNumber = compNumber();
+//compare userGuess to randNumber;
  var compare = function(){
-    if (numberOne > compNumber){
-     console.log("That is too high");
+    if (numberOne() > randNumber){
+     return("That is too high");
    }
-   else if (numberOne < compNumber) {
-     console.log("That is too low");
+   else if (numberOne() < randNumber) {
+     return("That is too low");
    }
-   else if (numberOne === compNumber){
-     console.log("Winner winner chicken dinner");
+   else if (numberOne() === randNumber){
+     return("Winner winner chicken dinner");
    }
    else{
-     console.log("Make sure your number is between 0 & 100");
+     return("Make sure your input is a number");
    }
  };
+//identify line to print message to
+var printMessage = document.getElementById('higherLower');
+//declare function to print message
+function printMessageHere () {
+  var message1 = compare();
+  printMessage.innerText = message1;
+}
+//add event listener
+guess.addEventListener('click', printMessageHere);
+
+console.log(userGuess);
